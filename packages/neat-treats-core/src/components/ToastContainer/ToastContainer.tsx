1ffd80558a9c2884f763a405c96fd52dc3141a6a
Steven Mancine <stevenrmancine@gmail.com>
2023-03-18 15:15:47 -0600

    Add borders

--- packages/neat-treats-core/src/components/ToastContainer/ToastContainer.tsx
+++ packages/neat-treats-core/src/components/ToastContainer/ToastContainer.tsx
@@ -1,21 +1,34 @@
+import { NTToastPositions } from '../../contexts/ToastContext';
 import { Portal } from '../Portal/Portal';
 import { Toast } from '../Toast/Toast';
-
+import './ToastContainer.scss';
 export type NTToastContainerProps = {
   className?: string;
   toasts: string[];
 };
 
+const positions: NTToastPositions[] = [
+  'top-left',
+  'top-center',
+  'top-right',
+  'bottom-left',
+  'bottom-center',
+  'bottom-right',
+];
+
 export const ToastContainer = ({
-  className,
-  toasts,
+  className = '',
+  toasts = [],
 }: NTToastContainerProps) => {
   return (
     <Portal container={document.body}>
       <div className={`nt-toast-container ${className}`}>
-        {toasts.map((el, i) => (
-          <Toast content={el} key={i} />
+        {positions.map((position) => (
+          <div className={`nt-toast-container--${position}`}>{position}</div>
         ))}
+        {/* {toasts.map((el, i) => (
+          <Toast content={el} key={i} />
+        ))} */}
       </div>
     </Portal>
   );
--- packages/neat-treats-core/src/contexts/ToastContext.tsx
+++ packages/neat-treats-core/src/contexts/ToastContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from 'react';
 import { ToastContainer } from '../components/ToastContainer/ToastContainer';
+
 export const ToastContext = createContext<
   React.Dispatch<React.SetStateAction<string[]>>
 >(() => null);
@@ -8,11 +9,18 @@ export type NTToastContextProviderdProps = {
   children: ReactNode;
 };
 
+export type NTToastPositions = `${YPositions}-${XPositions}`;
+
+export type NTToast = {
+  msg: string;
+  id: string;
+  position: NTToastPositions;
+};
+
 export const ToastContextProvider = ({
   children,
 }: NTToastContextProviderdProps) => {
   const [toasts, setToasts] = useState<string[]>([]);
-  console.log(toasts);
   return (
     <ToastContext.Provider value={setToasts}>
       {children}
